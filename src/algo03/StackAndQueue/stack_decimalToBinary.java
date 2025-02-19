ad353eec18ec23b22c8f13eed5c01e825bd6a379
awesomepossumgirl <awesomepossumgirl1@gmail.com>
2025-02-19 17:02:20 +0900

    edit stack_decimalToBinary

--- src/algo03/StackAndQueue/stack_decimalToBinary.java
+++ src/algo03/StackAndQueue/stack_decimalToBinary.java
@@ -2,7 +2,7 @@ package algo03.StackAndQueue;
 
 import java.util.Stack;
 
-public class queue_decimalToBinary {
+public class stack_decimalToBinary {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
