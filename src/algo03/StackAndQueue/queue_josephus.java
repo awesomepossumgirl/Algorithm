a6ad468c95b2a37163151b40ee09867c86cd5960
awesomepossumgirl <awesomepossumgirl1@gmail.com>
2025-02-20 13:28:14 +0900

    josephus code edit

--- src/algo03/StackAndQueue/queue_josephus.java
+++ src/algo03/StackAndQueue/queue_josephus.java
@@ -1,42 +1,40 @@
 package algo03.StackAndQueue;
 
 import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.Queue;
 
 public class queue_josephus {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		int K = in.nextInt();
-		in.close();
+    public static void main(String[] args) {
+        System.out.println(solution(5, 2)); // 결과: 3
+    }
 
-		System.out.println(solution(N, K));
+    /**
+     * 요세푸스 문제를 해결하는 함수
+     * 
+     * @param N 사람의 수 (1부터 N까지 번호가 있음)
+     * @param K 제거할 순번 (K번째 사람을 계속 제거함)
+     * @return 마지막에 살아남은 사람의 번호
+     */
+    public static int solution(int N, int K) {
+        // 큐(Queue) 생성: 원형 구조를 시뮬레이션하기 위해 사용
+        Queue<Integer> queue = new LinkedList<>();
 
-		// 테스트 케이스
-		System.out.println(solution(7, 3)); // <3, 6, 2, 7, 5, 1, 4>
-		System.out.println(solution(5, 2)); // <2, 4, 1, 5, 3>
-	}
+        // 1부터 N까지의 사람을 큐에 추가
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
 
-	public static String solution(int N, int K) {
-		// 백준 1158번 요세푸스
-		// https://www.acmicpc.net/problem/1158
-		
-		LinkedList<Integer> list = new LinkedList<>();
-		for (int i = 1; i <= N; i++) {
-			list.add(i);
-		}
+        // 마지막 한 명이 남을 때까지 반복
+        while (queue.size() > 1) {
+            // K번째 사람을 제거하기 위해 K-1번 앞의 사람을 뒤로 보냄
+            for (int i = 1; i < K; i++) {
+                queue.add(queue.poll()); // 맨 앞 사람을 빼서 큐의 뒤에 추가
+            }
+            queue.poll(); // K번째 사람 제거
+        }
 
-		StringBuilder sb = new StringBuilder();
-		sb.append('<');
-
-		int index = 0;
-		while (list.size() > 1) {
-			index = (index + (K - 1)) % list.size();
-			sb.append(list.remove(index)).append(", ");
-		}
-
-		sb.append(list.remove()).append('>');
-		return sb.toString();
-	}
+        // 마지막 남은 사람 반환
+        return queue.poll();
+    }
 }
