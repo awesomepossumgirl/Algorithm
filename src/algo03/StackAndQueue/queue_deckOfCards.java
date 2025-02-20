bca391a148c5110b76f3cbd82ef2eaf5cb8d4f71
awesomepossumgirl <awesomepossumgirl1@gmail.com>
2025-02-20 16:52:11 +0900

    edit deckOfCards

--- src/algo03/StackAndQueue/queue_deckOfCards.java
+++ src/algo03/StackAndQueue/queue_deckOfCards.java
@@ -6,33 +6,36 @@ import java.util.Deque;
 
 public class queue_deckOfCards {
 
-    public static void main(String[] args) {
-        // 테스트 케이스
-        System.out.println(solution(
-            new String[]{"a", "b", "c"}, 
-            new String[]{"d", "e", "f"}, 
-            new String[]{"a", "d", "b", "e", "c", "f"})); // Yes
-        
-        System.out.println(solution(
-            new String[]{"a", "b", "c"}, 
-            new String[]{"d", "e", "f"}, 
-            new String[]{"a", "b", "d", "e", "c", "f"})); // No
-    }
-        public static String solution(String[] cards1, String[] cards2, String[] goal) {
-            Deque<String> cardsDeque1 = new ArrayDeque<>(Arrays.asList(cards1));
-            Deque<String> cardsDeque2 = new ArrayDeque<>(Arrays.asList(cards2));
+	public static void main(String[] args) {
+		// 테스트 케이스
+		System.out.println(solution(
+				new String[] { "a", "b", "c" }, 
+				new String[] { "d", "e", "f" },
+				new String[] { "a", "d", "b", "e", "c", "f" })); // Yes
 
-            // goal 배열을 순회하면서 비교
-            for (String target : goal) {
-                if (!cardsDeque1.isEmpty() && cardsDeque1.peek().equals(target)) {
-                    cardsDeque1.poll();
-                } else if (!cardsDeque2.isEmpty() && cardsDeque2.peek().equals(target)) {
-                    cardsDeque2.poll(); 
-                } else {
-                    return "No";
-                }
-            }
-            return "Yes"; // 모든 목표 문자열을 만들 수 있으면 성공
-        
-    }
+		System.out.println(solution(
+				new String[] { "a", "b", "c" }, 
+				new String[] { "d", "e", "f" },
+				new String[] { "a", "b", "d", "e", "c", "f" })); // No
+	}
+
+	public static String solution(String[] cards1, String[] cards2, String[] goal) {
+		// 프로그래머스 카두 뭉치 문제
+
+		Deque<String> cardsDeque1 = new ArrayDeque<>(Arrays.asList(cards1));
+		Deque<String> cardsDeque2 = new ArrayDeque<>(Arrays.asList(cards2));
+
+		// goal 배열을 순회하면서 비교
+		for (String target : goal) {
+			if (!cardsDeque1.isEmpty() && cardsDeque1.peek().equals(target)) {
+				cardsDeque1.poll();
+			} else if (!cardsDeque2.isEmpty() && cardsDeque2.peek().equals(target)) {
+				cardsDeque2.poll();
+			} else {
+				return "No";
+			}
+		}
+		return "Yes"; // 모든 목표 문자열을 만들 수 있으면 성공
+
+	}
 }
